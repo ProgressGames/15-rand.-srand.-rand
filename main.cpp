8e34b2608603a0dd27b8c9e888dc9a721e2e53fd
Terrarich <163340619+terrarich@users.noreply.github.com>
2024-09-29 18:06:54 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+// Генератор случ. чисел
+
+int main() {
+    int const SIZE = 10;
+    int arr[SIZE];
+    srand(time(NULL));
+    for (int i= 0;i< SIZE;++i)
+    {
+        arr[i] = rand() % 10;
+    }
+
+    for (int i = 0;i < SIZE;++i)
+    {
+        cout << arr[i] << endl;
+    }
+}
